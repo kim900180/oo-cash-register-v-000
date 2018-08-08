e60b276646a567725de8faf93d836ae235eb0753
Jae Yong Kim <kim900180@gmail.com>
2018-08-08 18:26:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,4 +6,16 @@ class CashRegister
     @discount = discount
     @items = []
   end
+
+  def add_item(title, price, quantity = 1)
+    if quantity > 1
+      i = 0
+      while i < quantity
+        @items << title
+        i += 1
+      end
+    else
+      @items << title
+    end
+  end
 end
